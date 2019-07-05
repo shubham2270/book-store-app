05422d83fca9b0651f0c27fce2fd5a676dcb7e5b
Shubham Kumar <shubham2270@gmail.com>
2019-07-05 19:27:40 +0530

    Updates

---
+++ precache-manifest.4721aa8548431d8f674bccddada6bc90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f8b5dc3e4579db61cfabee0225d6b9b",
+    "url": "/book-store-app/index.html"
+  },
+  {
+    "revision": "938180ea2ee8727cc066",
+    "url": "/book-store-app/static/css/main.6e58ee08.chunk.css"
+  },
+  {
+    "revision": "b636eaff3c564cc0af30",
+    "url": "/book-store-app/static/js/2.b96f2cdc.chunk.js"
+  },
+  {
+    "revision": "938180ea2ee8727cc066",
+    "url": "/book-store-app/static/js/main.6ebe1201.chunk.js"
+  },
+  {
+    "revision": "6ca486ca6719dca5e54d",
+    "url": "/book-store-app/static/js/runtime~main.d78d8906.js"
+  }
+]);
